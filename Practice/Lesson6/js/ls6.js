8e03aca035ed147c9274e02d50f31bdec86d77ba
sami <samuelghirmay3@gmail.com>
2022-11-10 03:04:42 -0600

    asfg

---
+++ Practice/Lesson6/js/ls6.js
@@ -0,0 +1,26 @@
+
+    const form=document.getElementById("form");
+    const emailField=document.getElementById("email");
+    const passwordField=document.getElementById("password");
+   
+    form.onsubmit=function () {
+      alert(`
+      email Address: ${emailField.value}
+      password is: ${passwordField.value}
+      `
+      );    
+    }
+
+    form.onsubmit=function(){
+        const output=document.getElementById("outputs");
+
+        // Event.defaultPrevented()
+
+        output.innerHTML=  `
+        email Address: ${emailField.value}
+        password is: ${passwordField.value}
+        `
+
+    }
+
+
